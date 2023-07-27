515767e5ce2de1c8c88f0e8aeb261e4d10b0dfd7
Fury0012 <marklstrawn350@gmail.com>
2023-07-27 12:44:51 +0100

    done

---
+++ Week2/Day 4/Core/likes/script.js
@@ -0,0 +1,31 @@
+var likes1 = 3;
+var likecountt1 = document.querySelector("#count1")
+
+function like1() {
+    likes1++;
+    likecountt1.innerText = likes1 + " Like(s)"
+    console.log("likecount1")
+}
+
+
+
+
+var likes2 = 12;
+var likecountt2 = document.querySelector("#count2")
+
+function like2() {
+    likes2++;
+    likecountt2.innerText = likes2 + " Like(s)"
+    console.log("likecountt2")
+}
+
+
+
+var likes3 = 9;
+var likecountt3 = document.querySelector("#count3")
+
+function like3() {
+    likes3++;
+    likecountt3.innerText = likes3 + " Like(s)"
+    console.log("likecount3")
+}
